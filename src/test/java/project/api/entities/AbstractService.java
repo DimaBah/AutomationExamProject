71b18e550d523348b65eb1eadcf89cf3cb91a4d9
Bah Dima <bahdiman@mail.ru>
2020-06-19 01:39:56 +0300

    Revert "1st practice commit in Exam Project"
    This reverts commit 85edfb90f6c22dc7b780d7d5cfa9b2e9d690a391.

--- src/test/java/project/api/entities/AbstractService.java
+++
@@ -1,6 +0,0 @@
-package project.api.entities;
-
-public class AbstractService {
-    protected String baseUrl = "https://swapi.dev/api";
-    protected String practiceUrl = "https://reqres.in/api";
-}
